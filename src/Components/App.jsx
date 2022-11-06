ee37418dbc305e1c76c5e485300f7f953292b8a2
Chavda2772 <chavdamahesh2772@gmail.com>
2022-11-06 11:47:55 +0530

    start game completed

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,33 +1,10 @@
 import { Component } from 'react';
-import Header from './Header';
-import Form from './Form';
-import CardList from './CardList';
+import StarMatch from './StartMatch';
 import './App.css';
 
 class App extends Component {
-  state = {
-    userDetails: [],
-  };
-
-  // Helper function
-  onAddUserHandler = (username) => {
-    fetch(`https://api.github.com/users/${username}`)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.message == 'Not Found') return;
-        let resData = [...this.state.userDetails, data];
-        this.setState({ userDetails: resData });
-      });
-  };
-
   render() {
-    return (
-      <>
-        <Header />
-        <Form addUser={this.onAddUserHandler} />
-        <CardList userDetails={this.state.userDetails} />
-      </>
-    );
+    return <StarMatch />;
   }
 }
 
--- src/Components/Header.jsx
+++
@@ -1,5 +0,0 @@
-function Header() {
-  return <h1>The Github card App</h1>;
-}
-
-export default Header;
